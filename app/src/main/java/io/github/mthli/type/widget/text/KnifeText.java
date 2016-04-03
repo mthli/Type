87839a33a0f6be996455d040bb376f694f9c4a50
Matthew Lee <matthewlee0725@gmail.com>
2016-04-03 15:31:44 +0800

    update

---
+++ app/src/main/java/io/github/mthli/type/widget/text/KnifeText.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright 2016 Matthew Lee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.github.mthli.type.widget.text;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.IntDef;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Spanned;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.util.AttributeSet;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KnifeText extends AppCompatEditText {
+    public static final int FORMAT_BOLD = 0x01;
+    public static final int FORMAT_ITALIC = 0x02;
+    public static final int FORMAT_UNDERLINED = 0x03;
+    public static final int FORMAT_STRIKETHROUGH = 0x04;
+
+    @IntDef({FORMAT_BOLD, FORMAT_ITALIC, FORMAT_UNDERLINED, FORMAT_STRIKETHROUGH})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface FormatValue {}
+
+    public KnifeText(Context context) {
+        super(context);
+    }
+
+    public KnifeText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public KnifeText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void bold(boolean valid) {
+        if (valid) {
+            styleValid(Typeface.BOLD, getSelectionStart(), getSelectionEnd());
+        } else {
+            styleInvalid(Typeface.BOLD, getSelectionStart(), getSelectionEnd());
+        }
+    }
+
+    public void italic(boolean valid) {
+        if (valid) {
+            styleValid(Typeface.ITALIC, getSelectionStart(), getSelectionEnd());
+        } else {
+            styleInvalid(Typeface.ITALIC, getSelectionStart(), getSelectionEnd());
+        }
+    }
+
+    private void styleValid(int style, int start, int end) {
+        switch (style) {
+            case Typeface.NORMAL:
+            case Typeface.BOLD:
+            case Typeface.ITALIC:
+            case Typeface.BOLD_ITALIC:
+                break;
+            default:
+                return;
+        }
+
+        if (start < end) {
+            getEditableText().setSpan(new StyleSpan(style), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+    }
+
+    private void styleInvalid(int style, int start, int end) {
+        switch (style) {
+            case Typeface.NORMAL:
+            case Typeface.BOLD:
+            case Typeface.ITALIC:
+            case Typeface.BOLD_ITALIC:
+                break;
+            default:
+                return;
+        }
+
+        if (start >= end) {
+            return;
+        }
+
+        StyleSpan[] spans = getEditableText().getSpans(start, end, StyleSpan.class);
+        List<KnifePart> list = new ArrayList<>();
+
+        for (StyleSpan span : spans) {
+            if (span.getStyle() == style) {
+                list.add(new KnifePart(getEditableText().getSpanStart(span), getEditableText().getSpanEnd(span)));
+                getEditableText().removeSpan(span);
+            }
+        }
+
+        for (KnifePart part : list) {
+            if (part.isValid()) {
+                if (part.getStart() < start) {
+                    styleValid(style, part.getStart(), start);
+                }
+
+                if (part.getEnd() > end) {
+                    styleValid(style, end, part.getEnd());
+                }
+            }
+        }
+    }
+
+    private boolean containStyle(int style, int start, int end) {
+        switch (style) {
+            case Typeface.NORMAL:
+            case Typeface.BOLD:
+            case Typeface.ITALIC:
+            case Typeface.BOLD_ITALIC:
+                break;
+            default:
+                return false;
+        }
+
+        if (start > end) {
+            return false;
+        }
+
+        if (start == end) {
+            if (start - 1 < 0 || start + 1 > getEditableText().length()) {
+                return false;
+            } else {
+                StyleSpan[] before = getEditableText().getSpans(start - 1, start, StyleSpan.class);
+                StyleSpan[] after = getEditableText().getSpans(start, start + 1, StyleSpan.class);
+                return before.length > 0 && after.length > 0 && before[0].getStyle() == style && after[0].getStyle() == style;
+            }
+        } else {
+            StringBuilder builder = new StringBuilder();
+
+            // Make sure no duplicate characters be added
+            for (int i = start; i < end; i++) {
+                StyleSpan[] spans = getEditableText().getSpans(i, i + 1, StyleSpan.class);
+                for (StyleSpan span : spans) {
+                    if (span.getStyle() == style) {
+                        builder.append(getEditableText().subSequence(i, i + 1).toString());
+                        break;
+                    }
+                }
+            }
+
+            return getEditableText().subSequence(start, end).toString().equals(builder.toString());
+        }
+    }
+
+    public void underline(boolean valid) {
+        if (valid) {
+            underlineValid(getSelectionStart(), getSelectionEnd());
+        } else {
+            underlineInvalid(getSelectionStart(), getSelectionEnd());
+        }
+    }
+
+    private void underlineValid(int start, int end) {
+        if (start < end) {
+            getEditableText().setSpan(new UnderlineSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+    }
+
+    private void underlineInvalid(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        UnderlineSpan[] spans = getEditableText().getSpans(start, end, UnderlineSpan.class);
+        List<KnifePart> list = new ArrayList<>();
+
+        for (UnderlineSpan span : spans) {
+            list.add(new KnifePart(getEditableText().getSpanStart(span), getEditableText().getSpanEnd(span)));
+            getEditableText().removeSpan(span);
+        }
+
+        for (KnifePart part : list) {
+            if (part.isValid()) {
+                if (part.getStart() < start) {
+                    underlineValid(part.getStart(), start);
+                }
+
+                if (part.getEnd() > end) {
+                    underlineValid(end, part.getEnd());
+                }
+            }
+        }
+    }
+
+    private boolean containUnderline(int start, int end) {
+        if (start > end) {
+            return false;
+        }
+
+        if (start == end) {
+            if (start - 1 < 0 || start + 1 > getEditableText().length()) {
+                return false;
+            } else {
+                UnderlineSpan[] before = getEditableText().getSpans(start - 1, start, UnderlineSpan.class);
+                UnderlineSpan[] after = getEditableText().getSpans(start, start + 1, UnderlineSpan.class);
+                return before.length > 0 && after.length > 0;
+            }
+        } else {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = start; i < end; i++) {
+                if (getEditableText().getSpans(i, i + 1, UnderlineSpan.class).length > 0) {
+                    builder.append(getEditableText().subSequence(i, i + 1).toString());
+                }
+            }
+
+            return getEditableText().subSequence(start, end).toString().equals(builder.toString());
+        }
+    }
+
+    public void strikethrough(boolean valid) {
+        if (valid) {
+            strikethroughValid(getSelectionStart(), getSelectionEnd());
+        } else {
+            strikethroughInvalid(getSelectionStart(), getSelectionEnd());
+        }
+    }
+
+    private void strikethroughValid(int start, int end) {
+        if (start < end) {
+            getEditableText().setSpan(new StrikethroughSpan(), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+    }
+
+    private void strikethroughInvalid(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        StrikethroughSpan[] spans = getEditableText().getSpans(start, end, StrikethroughSpan.class);
+        List<KnifePart> list = new ArrayList<>();
+
+        for (StrikethroughSpan span : spans) {
+            list.add(new KnifePart(getEditableText().getSpanStart(span), getEditableText().getSpanEnd(span)));
+            getEditableText().removeSpan(span);
+        }
+
+        for (KnifePart part : list) {
+            if (part.isValid()) {
+                if (part.getStart() < start) {
+                    strikethroughValid(part.getStart(), start);
+                }
+
+                if (part.getEnd() > end) {
+                    strikethroughValid(end, part.getEnd());
+                }
+            }
+        }
+    }
+
+    private boolean containStrikethrough(int start, int end) {
+        if (start > end) {
+            return false;
+        }
+
+        if (start == end) {
+            if (start - 1 < 0 || start + 1 > getEditableText().length()) {
+                return false;
+            } else {
+                StrikethroughSpan[] before = getEditableText().getSpans(start - 1, start, StrikethroughSpan.class);
+                StrikethroughSpan[] after = getEditableText().getSpans(start, start + 1, StrikethroughSpan.class);
+                return before.length > 0 && after.length > 0;
+            }
+        } else {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = start; i < end; i++) {
+                if (getEditableText().getSpans(i, i + 1, StrikethroughSpan.class).length > 0) {
+                    builder.append(getEditableText().subSequence(i, i + 1).toString());
+                }
+            }
+
+            return getEditableText().subSequence(start, end).toString().equals(builder.toString());
+        }
+    }
+
+    public boolean contains(@FormatValue int format) {
+        switch (format) {
+            case FORMAT_BOLD:
+                return containStyle(Typeface.BOLD, getSelectionStart(), getSelectionEnd());
+            case FORMAT_ITALIC:
+                return containStyle(Typeface.ITALIC, getSelectionStart(), getSelectionEnd());
+            case FORMAT_UNDERLINED:
+                return containUnderline(getSelectionStart(), getSelectionEnd());
+            case FORMAT_STRIKETHROUGH:
+                return containStrikethrough(getSelectionStart(), getSelectionEnd());
+            default:
+                return false;
+        }
+    }
+
+    public void clearFormats() {
+        setText(getEditableText().toString());
+        setSelection(getEditableText().length());
+    }
+}
